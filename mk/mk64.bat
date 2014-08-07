210d909ec857bb069fd0244eac5fc9e84aed5500
NaldoDj <marinaldo.jesus@gmail.com>
2014-08-07 01:44:49 -0300

    revisao no script de compilacao quando win64

--- mk/mk64.bat
+++ mk/mk64.bat
@@ -5,39 +5,39 @@ SET _PATH=%PATH%
 SET _HB_PATH=%HB_PATH%
 SET HB_PATH=d:\hb32\
 SET MinGW64_PATH=d:\MinGW64\
-SET PATH=%HB_PATH%bin\
-SET path=%PATH%;%MinGW64_PATH%bin\
+SET PATH=%HB_PATH%bin\win\mingw64\
+SET PATH=%PATH%;%MinGW64_PATH%bin\
 
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_assignv.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_assignv_dyn_obj.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_dyn_obj.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_mt.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_mt_assignv.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_mt_assignv_dyn_obj.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_mt_dyn_obj.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_assignv.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_assignv_dyn_obj.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_assignv.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_dyn_obj.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_assignv.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_dyn_obj.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_dyn_obj_assignv.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_mt.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_mt_assignv.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_mt_dyn_obj.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_mt_dyn_obj_assignv.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_mt.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_mt_assignv.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_mt_assignv_dyn_obj.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_mt_dyn_obj.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dyn_obj.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_mt.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_mt_assignv.hbp
-    %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_mt_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_assignv.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_assignv_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_mt.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_mt_assignv.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_mt_assignv_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_array_mt_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_assignv.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_assignv_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_assignv.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_assignv.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_dyn_obj_assignv.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_mt.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_mt_assignv.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_mt_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_memio_mt_dyn_obj_assignv.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_mt.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_mt_assignv.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_mt_assignv_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dbfile_mt_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_dyn_obj.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_mt.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_mt_assignv.hbp
+    hbmk2.exe -strip- -jobs=10 -cpp -compr=max -comp=mingw64 ..\hbp\tBigNtst_mt_dyn_obj.hbp
 
 SET HB_PATH=%_HB_PATH%
 SET PATH=%_PATH%
--- mk/mkLib64.bat
+++ mk/mkLib64.bat
@@ -5,10 +5,10 @@ SET _PATH=%PATH%
 SET _HB_PATH=%HB_PATH%
 SET HB_PATH=d:\hb32\
 SET MinGW64_PATH=d:\MinGW64\
-SET PATH=%HB_PATH%bin\
-SET path=%PATH%;%MinGW64_PATH%bin\
+SET PATH=%HB_PATH%bin\win\mingw64\
+SET PATH=%PATH%;%MinGW64_PATH%bin\
 
-   %HB_PATH%bin\hbmk2.exe -strip- -jobs=10 -cpp -compr=no -comp=mingw64 ..\hbp\_tbigNumber.hbp	
+   hbmk2.exe -strip- -jobs=10 -cpp -compr=no -comp=mingw64 ..\hbp\_tbigNumber.hbp	
 
 SET HB_PATH=%_HB_PATH%
 SET PATH=%_PATH%
