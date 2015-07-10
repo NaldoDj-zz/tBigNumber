edf51b0b2ee08e6f55a8031642d9a257f1df761e
NaldoDj <marinaldo.jesus@gmail.com>
2015-07-10 13:17:47 -0300

    2015-07-10 13:17 UTC--300 NaldoDj (marinaldo/dot/jesus/at/blacktdn/dot/com)

--- mk/mkHB.bat
+++ mk/mkHB.bat
@@ -12,8 +12,17 @@ SET > env_mkHB.txt
     SET HB_PLATFORM=win
     SET HB_COMPILER=mingw
     SET HB_CCPATH=%MinGW_PATH%
+    rem SET HB_BUILD_VERBOSE=yes
        D:
        CD %HB_PATH%
+       IF "%1"=="-B" ( 
+            IF EXIST ".\bin\win\mingw" (
+                rmdir ".\bin\win\mingw" /s /q
+            )                
+            IF EXIST ".\lib\win\mingw" (
+                rmdir ".\lib\win\mingw" /s /q
+            )            
+       )
        %HB_PATH%\win-make.exe -f MakeFile %1
 D:
 CD D:\GitHub\tBigNumber\mk\
--- mk/mkHB64.bat
+++ mk/mkHB64.bat
@@ -12,8 +12,17 @@ SET > env_mkHB64.txt
     SET HB_PLATFORM=win
     SET HB_COMPILER=mingw64
     SET HB_CCPATH=%MinGW64_PATH%
+    rem SET HB_BUILD_VERBOSE=yes
        D:
        CD %HB_PATH%
+       IF "%1"=="-B" ( 
+            IF EXIST ".\bin\win\mingw64" (
+                rmdir ".\bin\win\mingw64" /s /q
+            )                
+            IF EXIST ".\lib\win\mingw64" (
+                rmdir ".\lib\win\mingw64" /s /q
+            )            
+       )
        %HB_PATH%\win-make.exe -f MakeFile %1
 D:
 CD D:\GitHub\tBigNumber\mk\
