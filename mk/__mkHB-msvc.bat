e227222ce6edc986f085f73966d5d4355add73d7
NaldoDj <marinaldo.jesus@blacktdn.com.br>
2019-03-16 11:45:08 -0300

    2019-03-16 11:41 UTC--300 NaldoDj (marinaldo.jesus blacktdn.com.br)

---
+++ mk/__mkHB-msvc.bat
@@ -0,0 +1,40 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+    echo BATCH FILE FOR Harbour msvc
+    rem ============================================================================
+    D:
+    CD D:\GitHub\tBigNumber\mk\
+    IF EXIST env_msvc.txt (
+        DEL env_msvc.txt /F /Q
+    )
+    SET > env_msvc.txt
+        IF EXIST "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+            CALL "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" X86
+            SET msvc_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin"
+        ) ELSE IF EXIST "c:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" (
+            CALL "c:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" X86
+            SET msvc_PATH="C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\bin"
+        ) ELSE IF EXIST "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+            CALL "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" X86
+            SET msvc_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin"
+        ) ELSE IF EXIST "c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
+            CALL "c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" X86
+            SET msvc_PATH="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin"
+        ) ELSE IF EXIST "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
+            CALL "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" X86
+            SET msvc_PATH="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin"
+        )
+        SET PATH=%msvc_PATH%;%HB_PATH%;%PATH%;
+        SET HB_CPU=x86_64
+        SET HB_PLATFORM=win
+        SET HB_COMPILER=msvc
+        SET HB_CCPATH=%msvc_PATH%
+        ..\..\core\bin\win\msvc\hbmk2 -plat=win -cpu=x86_64 -jobs=10 -cpp -compr=no  -comp=msvc -hbcppmm- ..\hbp\_tbigNumber.hbp
+        ..\..\core\bin\win\msvc\hbmk2 -plat=win -cpu=x86_64 -jobs=10 -cpp -compr=max -comp=msvc -hbcppmm- -gui ..\hbp\tBigNtst.hbp
+    D:
+    CD D:\GitHub\tBigNumber\mk\
+    for /f %%e in (env_msvc.txt) do (
+        SET %%e
+    )
+    DEL env_msvc.txt /F /Q
+ENDLOCAL
---
+++ mkvz/__mkHB-msvc64.bat
@@ -0,0 +1,50 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+    echo BATCH FILE FOR Harbour msvc64
+    rem ============================================================================
+    D:
+    CD D:\GitHub\tBigNumber\mkvz
+    IF EXIST env_msvc64.txt (
+        DEL env_msvc64.txt /F /Q
+    )
+    SET > env_msvc64.txt
+        SET HB_PATH=D:\GitHub\harbour-core\
+        IF EXIST "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+            CALL "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" X64
+            SET msvc64_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin"
+        ) ELSE IF EXIST "c:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" (
+            CALL "c:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" X64
+            SET msvc64_PATH="C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\bin"
+        ) ELSE IF EXIST "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+            CALL "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" X64
+            SET msvc64_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin"
+        ) ELSE IF EXIST "c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
+            CALL "c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" X64
+            SET msvc64_PATH="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin"
+        ) ELSE IF EXIST "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
+            CALL "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" X64
+            SET msvc64_PATH="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin"
+        )
+        SET PATH=%msvc64_PATH%;%HB_PATH%;%PATH%;
+        SET HB_CPU=x86_64
+        SET HB_PLATFORM=win
+        SET HB_COMPILER=msvc64
+        SET HB_CCPATH=%msvc64_PATH%
+        IF EXIST D:\OpenSSL-Win64\include (
+            SET HB_WITH_OPENSSL=D:\OpenSSL-Win64\include
+        )
+        IF EXIST D:\FreeImage\Dist\x64 (
+            SET HB_WITH_FREEIMAGE=D:\FreeImage\Dist\x64
+        )
+        IF EXIST D:\mxml (
+            SET HB_WITH_MXML=D:\mxml
+        )
+        ..\..\core\bin\win\msvc64\hbmk2 -plat=win -cpu=x86_64 -jobs=10 -cpp -compr=no  -comp=msvc64 -hbcppmm- ..\hbpvz\_tbigNumber.hbp
+        ..\..\core\bin\win\msvc64\hbmk2 -plat=win -cpu=x86_64 -jobs=10 -cpp -compr=max -comp=msvc64 -hbcppmm- -gui ..\hbpvz\tBigNtst.hbp
+    D:
+    CD D:\GitHub\tBigNumber\mkvz
+    for /f %%e in (env_msvc64.txt) do (
+        SET %%e
+    )
+    DEL env_msvc64.txt /F /Q
+ENDLOCAL
